aa7ede025d0c5a98e613d48ed7965bde90536b46
Bennett McAfee <105509324+bmcafee@users.noreply.github.com>
2026-06-09 10:51:47 -0400

    Revert all MLE changes

--- src/mleLoop.c
+++ src/mleLoop.c
@@ -6,21 +6,17 @@ void mleLoopCoe(double *alpha, double *doobs, double *c1, double *c2, double *be
 	double a1, z=0; // z has to be a double b/c kz is passed in as a double (for cases where it isn't 0)
 	// double alpha=*alpha, doobs=*doobs, c1=*c1, c2=*c2, beta=*beta, irr=*irr, wtr=*wtr, kz=*kz, dosat=*dosat;
 	int ni=*nobs;
-	double c1_pos = exp(*c1);
-	double c2_pos = exp(*c2);
 	for(i=1; i<ni; i++){
 
 		if(kz[i-1] != z){ // if k.gas is 0, kz is 0
 
-			// a1 = *c1 * irr[i - 1] + *c2 * log(wtr[i - 1]) + kz[i - 1] * dosat[i - 1];
-			a1 = c1_pos * irr[i - 1] - c2_pos * log(wtr[i - 1]) + kz[i - 1] * dosat[i - 1];
+			a1 = *c1 * irr[i - 1] + *c2 * log(wtr[i - 1]) + kz[i - 1] * dosat[i - 1];
 
 			alpha[i] = a1 / kz[i - 1] + -exp(-1 * kz[i - 1]) * a1 / kz[i - 1] + beta[i - 1] * alpha[i-1];
 
 		}else{ // if kz is 0, must use different equations to avoid /0
 
-			// alpha[i] = alpha[i-1] + *c1 * irr[i - 1] + *c2 * log(wtr[i - 1]);
-			alpha[i] = alpha[i-1] + c1_pos * irr[i - 1] - c2_pos * log(wtr[i - 1]);
+			alpha[i] = alpha[i-1] + *c1 * irr[i - 1] + *c2 * log(wtr[i - 1]);
 
 		}
 
@@ -32,22 +28,17 @@ void mleLoopCpe(double *alpha, double *doobs, double *c1, double *c2, double *be
 	double a1, z=0; // z has to be a double b/c kz is passed in as a double (for cases where it isn't 0)
 	// double alpha=*alpha, doobs=*doobs, c1=*c1, c2=*c2, beta=*beta, irr=*irr, wtr=*wtr, kz=*kz, dosat=*dosat;
 	int ni=*nobs;
-	double c1_pos = exp(*c1);
-	double c2_pos = exp(*c2);
 	for(i=1; i<ni; i++){
 
 		if(kz[i-1] != z){ // if k.gas is 0, kz is 0
 
-			// a1 = *c1 * irr[i - 1] + *c2 * log(wtr[i - 1]) + kz[i - 1] * dosat[i - 1];
-			a1 = c1_pos * irr[i - 1] - c2_pos * log(wtr[i - 1]) + kz[i - 1] * dosat[i - 1];
+			a1 = *c1 * irr[i - 1] + *c2 * log(wtr[i - 1]) + kz[i - 1] * dosat[i - 1];
 
 			alpha[i] = a1 / kz[i - 1] + -exp(-1 * kz[i - 1]) * a1 / kz[i - 1] + beta[i - 1] * doobs[i-1];
 
 		}else{ // if kz is 0, must use different equations to avoid /0
 
-			// alpha[i] = doobs[i-1] + *c1 * irr[i - 1] + *c2 * log(wtr[i - 1]);
-			alpha[i] = doobs[i-1] + c1_pos * irr[i - 1] - c2_pos * log(wtr[i - 1]);
-			
+			alpha[i] = doobs[i-1] + *c1 * irr[i - 1] + *c2 * log(wtr[i - 1]);
 
 		}
 
