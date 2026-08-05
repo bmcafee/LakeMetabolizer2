87c94604f1d8269c831d192e8f144d057fdd99e6
Bennett McAfee <105509324+bmcafee@users.noreply.github.com>
2026-08-05 15:49:14 -0400

    Update metab documentation

--- R/metab.R
+++ R/metab.R
@@ -7,7 +7,7 @@
 #'
 #'
 #'@usage
-#'metab(data, method, verbose = TRUE, wtr.name="wtr", irr.name="irr", 
+#'metab(data, method, verbose = interactive(), wtr.name="wtr", irr.name="irr", 
 #'      do.obs.name="do.obs", ...)
 #'
 #'@param data
@@ -109,7 +109,7 @@
 #'
 #'@export
 
-metab <- function(data, method, verbose = interactive(), wtr.name="wtr", irr.name="irr", do.obs.name="do.obs", ...){
+metab <- function(data, method = NULL, verbose = interactive(), wtr.name="wtr", irr.name="irr", do.obs.name="do.obs", ...){
 
 	m.args <- list(...)
 
